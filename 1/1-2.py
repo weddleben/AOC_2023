52163d9c31099af9df6fb33092a3ba869521ecc6
Ben Weddle <107364642+weddleben@users.noreply.github.com>
2023-12-02 14:41:44 -0600

    moving files around

--- 1/1-2.py
+++ 1/1-2.py
@@ -1,4 +1,5 @@
 numbers_as_words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+words_and_nums = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
 def open_file_and_populate_list():
     file = open(f"data.txt", "r")
@@ -12,5 +13,3 @@ def sanitize_data(lines):
             line = line.replace('\n', '')
         initial_nums.append(line)
     return initial_nums
-
-nums = sanitize_data(open_file_and_populate_list())
